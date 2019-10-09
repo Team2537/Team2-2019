89e33a66e0588d9f3fa70a1f017c40e107d8aaa6
Alex <rainsiphoner@gmail.com>
2019-10-09 13:32:05 -0400

    Shooter Command and Subsystems added

---
+++ src/main/java/frc/robot/ShooterStuff/ShooterSubsystem.java
@@ -0,0 +1,26 @@
+package frc.robot.ShooterStuff;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * This is going to be the subsystem for shooter It will contain variables used
+ * for shooter It may also contain public methods used for shooter
+ */
+
+public class ShooterSubsystem extends Subsystem{
+ 
+
+
+public ShooterSubsystem(){
+
+
+
+
+}
+
+    @Override
+    protected void initDefaultCommand() {
+        // TODO Auto-generated method stub
+
+    }
+}
